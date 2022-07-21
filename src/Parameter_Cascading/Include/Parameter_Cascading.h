005da73584a236dedf7d7a0bd6695d676cb171d4
Matteo Tomasetto <matteo.tomasetto10@gmail.com>
2022-07-21 19:20:35 +0200

    Minor change

--- src/Parameter_Cascading/Include/Parameter_Cascading.h
+++ src/Parameter_Cascading/Include/Parameter_Cascading.h
@@ -57,7 +57,7 @@ class Parameter_Cascading
 			{
 				// Compute the lambdas for the parameter cascading algorithm from the rhos introduced in \cite{Bernardi}
 				VectorXr rhos(13,1);
-				rhos << 0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95,0.99;
+				rhos << 0.01,0.025,0.05,0.075,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9;
 
 				unsigned int n = H.getModel().getRegressionData().getNumberofObservations();
 
